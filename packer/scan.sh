a20e04f8ccb981606e3702a28ff2530f411d9a19
Richard Bullington-McGuire <richard@moduscreate.com>
2018-06-11 18:33:35 -0400

    Make scanning script its own thing

---
+++ packer/scan.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Install openscap and run a security scan
+
+set -euo pipefail
+
+sudo yum -qy install openscap-scanner scap-security-guide
+
+# This will have a non-zero exit if any of the scans fail, so do not fail immediately on that
+set +e
+oscap xccdf eval --profile C2S --results scan-xccdf-results.xml /usr/share/xml/scap/ssg/content/ssg-centos7-xccdf.xml
+set -e
+
+oscap xccdf generate report --output scan-xccdf-report.html scan-xccdf-results.xml
