19d97c0a54f6a9aff1f5946eb59e1a26e441be68
Richard Bullington-McGuire <richard@moduscreate.com>
2018-06-11 15:58:16 -0400

    Fix up tty handling

--- packer/pack.sh
+++ packer/pack.sh
@@ -14,14 +14,21 @@ if [[ -n "$INTERFACE" ]]; then  # we are running on EC2
     PACKER_AWS_VPC_ID="$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$INTERFACE"/vpc-id)"
 fi
 
-docker run -i -t  \
+# Thanks https://stackoverflow.com/a/48230089
+USE_TTY=""
+test -t 1 && USE_TTY="-t"
+
+docker run -i \
+    ${USE_TTY}  \
     --env-file <(grep ^export ../env.sh | cut -c8-) \
     -e "PACKER_AWS_SUBNET_ID=$PACKER_AWS_SUBNET_ID" \
     -e "PACKER_AWS_VPC_ID=$PACKER_AWS_VPC_ID" \
     --mount type=bind,source="$(pwd)",target=/app \
     hashicorp/packer:light \
     validate app/machines/web-server.json
- docker run -i -t  \
+
+docker run -i  \
+    ${USE_TTY}  \
     --env-file <(grep ^export ../env.sh | cut -c8-) \
     -e "PACKER_AWS_SUBNET_ID=$PACKER_AWS_SUBNET_ID" \
     -e "PACKER_AWS_VPC_ID=$PACKER_AWS_VPC_ID" \
