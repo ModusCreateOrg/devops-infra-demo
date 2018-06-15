0b286e0f5c5fec4c61914f122fa8a5d2ea889a9a
Richard Bullington-McGuire <richard@moduscreate.com>
2018-06-15 17:30:46 -0400

    Get ansible through packer to start to run
    This still errors out but it is close

--- packer/bin/pack.sh
+++ packer/bin/pack.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+set -euo pipefail
+
+# Credit to Stack Overflow questioner Jiarro and answerer Dave Dopson
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# http://stackoverflow.com/a/246128/424301
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR/.."
 
 set +e
 # Technique for determining what subnet I live in on EC2 cribbed from:
--- packer/bin/scan.sh
+++ packer/bin/scan.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 
 sudo yum -qy install openscap-scanner scap-security-guide
 
+mkdir -p build
+cd build
 # This will have a non-zero exit if any of the scans fail, so do not fail immediately on that
 set +e
 oscap xccdf eval --profile C2S --results scan-xccdf-results.xml /usr/share/xml/scap/ssg/content/ssg-centos7-xccdf.xml
