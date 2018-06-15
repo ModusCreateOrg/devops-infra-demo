4d473cbf3c1f44de8e01754906ed03d91c2b4e0f
Richard Bullington-McGuire <richard@moduscreate.com>
2018-06-15 15:35:48 -0400

    DRY on docker command

--- packer/pack.sh
+++ packer/pack.sh
@@ -14,24 +14,25 @@ if [[ -n "$INTERFACE" ]]; then  # we are running on EC2
     PACKER_AWS_VPC_ID="$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$INTERFACE"/vpc-id)"
 fi
 
+# Clean up the env file for use in packer
+TMPFILE="$(mktemp)"
+grep ^export ../env.sh | cut -c8- > "$TMPFILE"
+
+# Only use TTY for Docker if we detect one, otherwise
+# this will balk when run in Jenkins
 # Thanks https://stackoverflow.com/a/48230089
 USE_TTY=""
 test -t 1 && USE_TTY="-t"
 
-docker run -i \
-    ${USE_TTY}  \
-    --env-file <(grep ^export ../env.sh | cut -c8-) \
-    -e "PACKER_AWS_SUBNET_ID=$PACKER_AWS_SUBNET_ID" \
-    -e "PACKER_AWS_VPC_ID=$PACKER_AWS_VPC_ID" \
-    --mount type=bind,source="$(pwd)",target=/app \
-    hashicorp/packer:light \
-    validate app/machines/web-server.json
+DOCKER_PACKER="docker run -i 
+    ${USE_TTY}  
+    --env-file $TMPFILE
+    -e PACKER_AWS_SUBNET_ID=$PACKER_AWS_SUBNET_ID 
+    -e PACKER_AWS_VPC_ID=$PACKER_AWS_VPC_ID 
+    --mount type=bind,source=$(pwd),target=/app 
+    hashicorp/packer:light"
 
-docker run -i  \
-    ${USE_TTY}  \
-    --env-file <(grep ^export ../env.sh | cut -c8-) \
-    -e "PACKER_AWS_SUBNET_ID=$PACKER_AWS_SUBNET_ID" \
-    -e "PACKER_AWS_VPC_ID=$PACKER_AWS_VPC_ID" \
-    --mount type=bind,source="$(pwd)",target=/app \
-    hashicorp/packer:light \
-    build app/machines/web-server.json
+$DOCKER_PACKER validate app/machines/web-server.json
+$DOCKER_PACKER build app/machines/web-server.json
+
+rm -f "$TMPFILE"
