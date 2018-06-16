60a673fc77eb6789afc9ecae9c5c2f384f2c4e01
Richard Bullington-McGuire <richard@moduscreate.com>
2018-06-16 10:30:12 -0400

    Simplify ansible provisioning
    Use shell to run ansible through vagrant to avoid `/tmp`
    since the playbook modifies that directory.

--- packer/bin/install-ansible.sh
+++ packer/bin/install-ansible.sh
@@ -7,9 +7,3 @@ IFS=$'\n\t'
 sudo yum -y -q install epel-release
 sudo yum -y -q install ansible
 ansible-galaxy install -r /app/ansible/requirements.yml
-# Thanks https://www.tricksofthetrades.net/2017/10/02/ansible-local-playbooks/
-#    and https://ubuntuforums.org/showthread.php?t=1294351
-cat <<EOF | sudo tee  /etc/ansible/hosts
-[local]
-localhost ansible_connection=local
-EOF
