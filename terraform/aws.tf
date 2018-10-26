2a99de886dac925516d0c493eda0ad3fa7a6ac4e
Richard Bullington-McGuire <richard@moduscreate.com>
2018-10-26 09:44:43 -0400

    Fix up format

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -11,10 +11,10 @@ output "account_id" {
   value = "${data.aws_caller_identity.current.account_id}"
 }
 
-#output "arn" {
-#  value = "${data.aws_caller_identity.current.arn}"
-#}
+output "arn" {
+  value = "${data.aws_caller_identity.current.arn}"
+}
 
-#output "user_id" {
-#  value = "${data.aws_caller_identity.current.user_id}"
-#}
+output "user_id" {
+  value = "${data.aws_caller_identity.current.user_id}"
+}
