295dc81d276c859fbec08c4ea0d5c60a55d3ae9e
Sergio Bruder <sergio@bruder.com.br>
2018-12-11 15:34:13 -0200

    initial codedeploy infra

---
+++ terraform/codedeploy.tf
@@ -0,0 +1,75 @@
+// devops-infra-demo
+resource "aws_iam_role" "infra-demo" {
+  name = "tf-infra-demo-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy-role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/tf-codedeploy-role"
+  role       = "${aws_iam_role.infra-demo.name}"
+}
+
+resource "aws_codedeploy_app" "infra-demo" {
+  name = "tf-infra-demo-app"
+}
+
+resource "aws_codedeploy_deployment_config" "infra-demo" {
+  deployment_config_name = "tf-infra-demo-deployment-config"
+
+  minimum_healthy_hosts {
+    type  = "HOST_COUNT"
+    value = 2
+  }
+}
+
+resource "aws_codedeploy_deployment_group" "infra-demo" {
+  app_name               = "${aws_codedeploy_app.infra-demo.name}"
+  deployment_group_name  = "dev"
+  service_role_arn       = "${aws_iam_role.infra-demo.arn}"
+  deployment_config_name = "${aws_codedeploy_deployment_config.infra-demo.id}"
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = "infra-demo-web"
+    }
+
+    ec2_tag_filter {
+      key   = "Project"
+      type  = "KEY_AND_VALUE"
+      value = "infra-demo"
+    }
+
+    ec2_tag_filter {
+      key   = "aws:autoscaling:groupName"
+      type  = "KEY_AND_VALUE"
+      value = "infra-demo-asg"
+    }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  alarm_configuration {
+    alarms  = ["my-alarm-name"]
+    enabled = true
+  }
+}
