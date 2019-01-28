c64f6da3a6f799bec56dcbf5b798afae4a223b16
Richard Bullington-McGuire <richard@moduscreate.com>
2019-01-27 22:28:45 -0500

    Add CSV to HTML converter

---
+++ bin/csv2html.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# Thanks Stack Overflow https://unix.stackexchange.com/a/105531/137901
+# Thanks https://divtable.com/table-styler/ for the free table CSS
+
+cat <<EOT
+<style>
+table.blueTable {
+  border: 1px solid #1C6EA4;
+  background-color: #EEEEEE;
+  width: 100%;
+  text-align: right;
+  border-collapse: collapse;
+}
+table.blueTable td, table.blueTable th {
+  border: 1px solid #AAAAAA;
+  padding: 3px 2px;
+}
+table.blueTable tbody td {
+  font-size: 13px;
+}
+table.blueTable tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.blueTable thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table.blueTable thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table.blueTable thead th:first-child {
+  border-left: none;
+}
+
+table.blueTable tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFFFFF;
+  background: #D0E4F5;
+  background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  border-top: 2px solid #444444;
+}
+table.blueTable tfoot td {
+  font-size: 14px;
+}
+table.blueTable tfoot .links {
+  text-align: right;
+}
+table.blueTable tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
+</style>
+<table class="blueTable">
+EOT
+print_header=true
+while read -r INPUT ; do
+    if $print_header; then
+        echo "<thead>"
+        el=th
+    else
+        el=td
+    fi
+    echo "<tr><$el>${INPUT//,/</$el><$el>}</$el></tr>"
+    if $print_header; then
+    echo "</thead>"
+        echo "<tbody>"
+        print_header=false
+    fi
+done
+echo "</tbody>"
+echo "</table>"
