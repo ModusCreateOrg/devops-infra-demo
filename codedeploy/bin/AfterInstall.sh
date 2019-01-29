a8dee19b0705a42b1df75be2d43e9186672bb5ed
Richard Bullington-McGuire <richard@moduscreate.com>
2019-01-28 19:38:06 -0500

    Ensure we source virtualenv activate

--- codedeploy/bin/AfterInstall.sh
+++ codedeploy/bin/AfterInstall.sh
@@ -35,7 +35,8 @@ NEWRELIC_LICENSE_KEY=$(aws secretsmanager get-secret-value \
     --secret-id newrelic_license \
     --output text \
     --query '[SecretString]')
-${VENV_DIR}/bin/activate
+#shellcheck disable=SC1090
+source ${VENV_DIR}/bin/activate
 newrelic-admin generate-config "${NEWRELIC_LICENSE_KEY}" "${NEWRELIC_CONFIG_DIR}/newrelic.ini.orig"
 sed 's/^app_name =.*$/app_name = Spin/' "${NEWRELIC_CONFIG_DIR}/newrelic.ini.orig" > "${NEWRELIC_CONFIG_DIR}/newrelic.ini"
 #rm -f "${NEWRELIC_CONFIG_DIR}/newrelic.ini.orig"
