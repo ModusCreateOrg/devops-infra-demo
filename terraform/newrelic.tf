a096dccc6ce1419d494f85d04d1e994ea3acf5a5
Richard Bullington-McGuire <richard@moduscreate.com>
2019-08-02 16:40:39 -0400

    Fix terraform fmt Fix Terrafmr format#

--- terraform/newrelic.tf
+++ terraform/newrelic.tf
@@ -9,7 +9,7 @@ provider "newrelic" {
 
 # Create an alert policy
 resource "newrelic_alert_policy" "alert" {
-  name = "Alert"
+  name  = "Alert"
   count = "${length(var.newrelic_apm_entities) > 0 ? (var.newrelic_alerts ? 1 : 0) : 0}"
 }
 
