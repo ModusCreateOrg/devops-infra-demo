7e20c4eb52262cb2e32d9a4c22d2ad0d95ca94f2
Akash Agrawal <akagr@outlook.com>
2022-08-23 11:11:28 +0530

    DOPS-101 Add bootstrap module for terraform project

---
+++ terraform/bootstrap/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "Amazon Region to use"
+  default     = "us-east-1"
+}
+
+variable "account_alias" {
+  description = "Prefix for backend resources"
+  default     = "moduscreate-devops-demo"
+}
